c9e853ff99134b8922645fe71d54cb1110877726
JYlsc <1052861935@qq.com>
2017-12-29 11:51:51 +0800

    调试

--- src/main/image_style_transfer/main.py
+++ src/main/image_style_transfer/main.py
@@ -9,6 +9,7 @@ from mxnet import gluon, autograd
 import mxnet as mx
 from mxnet.gluon.model_zoo import vision
 from src.main.image_style_transfer import model as model
+from src.main.image_style_transfer.model import FeatureNet
 from src.tool import net_tool as tool
 from src.main.image_style_transfer import loss as loss_function
 
@@ -22,47 +23,38 @@ learning_rate = 1
 # 迭代次数
 iter = 10000
 
+size = 400
+
 data_path = "../../../data/"
 
 # 设置输入输出文件路径
-input_path = data_path+"img/content/input.jpg"
-style_path = data_path+"img/style/style7.jpg"
-output_path =data_path+"img/output/"
+input_path = data_path + "img/content/input.jpg"
+style_path = data_path + "img/style/style7.jpg"
+output_path = data_path + "img/output/"
 
 # vgg 参数路径
-param = data_path+"param/"
+param = data_path + "param/"
 
 
-def train():
-    # 加载vgg模型
-    vgg = vision.vgg19(ctx=ctx, pretrained=True, root=param)
-
-    # 构建风格网络及内容网络
-    features_net = model.features_net(vgg)
-
-    # 读取图片
-    img = tool.read_img(input_path).as_in_context(ctx)
-    style_img = tool.read_img(style_path).as_in_context(ctx)
-
+def style_transfer(net, content_img, style_img):
     # 获取style及content
-    content = features_net(img)[0]
-    style = features_net(style_img)[1:]
+    content = net(content_img)[0]
+    style = net(style_img)[1:]
 
-    output = gluon.Parameter('_img', shape=img.shape)
+    output = gluon.Parameter('_img', shape=content_img.shape)
     output.initialize(ctx=ctx)
-    output.set_data(img)
+    output.set_data(content_img)
 
-    tool.save_img(output.data(), output_path+"src.jpg")
     trainer = gluon.Trainer([output], 'adam', {'learning_rate': learning_rate})
 
     # 迭代获取新图片
     for e in range(iter):
         with autograd.record():
             _img = output.data()
-            _features = features_net(_img)
+            _features = net(_img)
             _content = _features[0]
             _style = _features[1:]
-            loss = loss_function.loss(content,_content,style,_style,alpha)
+            loss = net.get_loss(content, _content) + alpha * net.get_style_loss(_style, style)
 
         loss.backward()
         trainer.step(1)
@@ -71,13 +63,18 @@ def train():
 
         if e % 100 == 0:
             tool.save_img(output.data(), output_path + str(e) + ".png")
-    tool.save_img(output.data(), output_path+"result.png")
+    tool.save_img(output.data(), output_path + "result.png")
 
 
+if __name__ == "__main__":
+    # 加载vgg模型
+    vgg = vision.vgg19(ctx=ctx, pretrained=True, root=param)
 
-train()
+    # 构建风格网络及内容网络
+    net = FeatureNet(vgg)
 
+    # 读取图片
+    content_img = tool.read_img(input_path, ctx=ctx, size=size)
+    style_img = tool.read_img(style_path, ctx=ctx, size=size)
 
-
-if __name__ =="__main__":
-    pass
+    style_transfer(net, content_img, input_path)
--- src/main/image_style_transfer/model.py
+++ src/main/image_style_transfer/model.py
@@ -8,30 +8,51 @@
 
 import mxnet as mx
 from mxnet import gluon
+from mxnet import ndarray as nd
 
 
 class FeatureNet():
 
-    def __init__(self,vgg):
+    def __init__(self, vgg):
+        self.net = self.init_net(vgg)
         pass
 
+    def init_net(self, vgg):
+        # 获取要输出的层对应symbol列表
+        data = mx.sym.var('data')
+        internals = vgg(data).get_internals()
+        print(internals.list_outputs())
+        list = [internals['vgg0_conv9_fwd_output'],
+                internals['vgg0_conv0_fwd_output'],
+                internals['vgg0_conv2_fwd_output'],
+                internals['vgg0_conv4_fwd_output'],
+                internals['vgg0_conv8_fwd_output'],
+                internals['vgg0_conv12_fwd_output']]
 
+        # 根据原有网络构建新的网络
+        net = gluon.SymbolBlock(list, data, params=vgg.collect_params())
+        return net
 
+    def get_gram(self, feature):
+        """
+        计算features 的 gram矩阵
+        :param features:
+        :return:
+        """
+        (b, ch, h, w) = feature.shape
+        features = feature.reshape((b, ch, w * h))
+        gram = nd.batch_dot(features, features, transpose_b=True)
+        return gram
 
-def features_net(vgg):
-    # 获取要输出的层对应symbol列表
-    data = mx.sym.var('data')
-    internals = vgg(data).get_internals()
-    print(internals.list_outputs())
-    list = [internals['vgg0_conv9_fwd_output'],
-            internals['vgg0_conv0_fwd_output'],
-            internals['vgg0_conv2_fwd_output'],
-            internals['vgg0_conv4_fwd_output'],
-            internals['vgg0_conv8_fwd_output'],
-            internals['vgg0_conv12_fwd_output']]
+    def get_loss(self, x, y):
+        return gluon.loss.L2Loss()(x, y)
 
-    # 根据原有网络构建新的网络
-    net = gluon.SymbolBlock(list, data, params=vgg.collect_params())
-    return net
-
-
+    def get_style_loss(self, features, _features):
+        loss = 0.
+        for i in range(len(features)):
+            G = self.get_gram(_features[i])
+            A = self.get_gram(features[i])
+            N = features[i].shape[1]
+            M = features[i] / N
+            loss = loss + self.get_loss(A, G) * (1. / (2 * (N ** 2) * (M ** 2))) * 0.2
+        return loss
